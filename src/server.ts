ae932db9cc6f92a567b2954dc0142cdc60b52cc3
fenndev <fenndev@protonmail.com>
2022-10-19 01:42:20 -0700

    Added environment variables

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+require('dotenv').config();
 
 const app = express();
 
@@ -6,6 +7,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000');
+app.listen(process.env.PORT, () => {
+    console.log(`Server started on port ${process.env.PORT}`);
 });
