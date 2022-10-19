c242f13ae04e7d5fdf6a3dd690015b4d5b665a39
fenndev <fenndev@protonmail.com>
2022-10-19 07:31:22 -0700

    Optimized full month name query

--- src/format-datetime.ts
+++ src/format-datetime.ts
@@ -12,21 +12,8 @@ function toTwentyFourHour(hour: number): string {
 }
 
 function fullMonthName(month: number): string {
-    switch (month) {
-        case 0: return 'January';
-        case 1: return 'February';
-        case 2: return 'March';
-        case 3: return 'April';
-        case 4: return 'May';
-        case 5: return 'June';
-        case 6: return 'July';
-        case 7: return 'August';
-        case 8: return 'September';
-        case 9: return 'October';
-        case 10: return 'November';
-        case 11: return 'December';
-        default: return 'Unknown';
-    }
+    const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    return monthNames[month];
 }
 
 function ndSuffix(num: number): string {
