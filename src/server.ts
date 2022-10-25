9c15af730dba8ec234885d58e9918e3c2dab832b
fenndev <fenndev@protonmail.com>
2022-10-24 22:29:03 -0700

    Removed unnessary components

--- src/server.ts
+++ src/server.ts
@@ -4,17 +4,9 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const lat = 55.7558;
-const lon = 37.6173;
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
 
 app.listen(process.env.PORT, async() => {
     console.log(`Server started on port ${process.env.PORT}`);
-    const weatherData = await queryWeather('Moscow');
-    console.log(weatherData);
 });
 
 app.get('/:query', async(req: any, res) => {
