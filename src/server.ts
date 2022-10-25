cf7a1070c3a38b7e54f1f85f652ac8521e3ad5bb
fenndev <fenndev@protonmail.com>
2022-10-24 21:08:59 -0700

    Update

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express, { query } from 'express';
 import WeatherData from './weather-data.js';
-import queryWeather from './fetch-weather.js';
+import queryWeather from './query-weather.js';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
