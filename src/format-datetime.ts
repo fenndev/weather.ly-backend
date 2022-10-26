ae9241e3799f1f33ff44199d5ce3947bcffd3ae2
fenndev <fenndev@protonmail.com>
2022-10-26 00:40:18 -0700

    Created monolithic server file

--- src/format-datetime.ts
+++
@@ -1,9 +0,0 @@
-import dayjs from 'dayjs';
-import utc from 'dayjs/plugin/utc.js';
-dayjs.extend(utc);
-
-function formatDateTime(utcTime: number, timezoneOffset: number): Date { 
-    return dayjs.utc(dayjs.unix(utcTime)).utcOffset(timezoneOffset / 60).toDate();
-}
-
-export default formatDateTime;
