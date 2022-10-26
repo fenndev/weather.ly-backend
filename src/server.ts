2914e2ee34e2bf7d9b6d2ee7a25321b44c76ae35
fenndev <fenndev@protonmail.com>
2022-10-26 00:14:31 -0700

    removed unneeded code

--- src/server.ts
+++ src/server.ts
@@ -5,14 +5,6 @@ dotenv.config();
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
-app.listen(process.env.PORT, async() => {
-    console.log(`Server started on port ${process.env.PORT}`);
-});
-
 export default async function queryHandler(req: any, res: any) {
     let queryString = req.params.query;
     let queryArray = queryString.split(',');
