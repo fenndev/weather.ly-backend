c599908100527d82d11d262bcbe58987a648d3ef
fenndev <fenndev@proton.me>
2023-02-11 03:39:05 -0800

    Added CORS middleware and query searching

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,28 @@
 import express, { query } from 'express';
+import cors from 'cors'
 import queryWeather from './query-weather.js';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+app.use(cors());
+
+app.get('/', async(req: any, res) => {
+  if(!req.query.q) res.send('Server online.');
+  else {
+    try {
+      let queryString = req.query.q;
+      let queryArray = queryString.split(',');
+      const weatherData = await queryWeather(queryArray);
+      res.send(weatherData);
+    }
+    catch (err) {
+      res.send(err);
+    }
+  }
+})
 
 app.listen(process.env.PORT, async() => {
     console.log(`Server started on port ${process.env.PORT}`);
-});
-
-app.get('/:query', async(req: any, res) => {
-  try {
-    let queryString = req.params.query;
-    let queryArray = queryString.split(',');
-    const weatherData = await queryWeather(queryArray);
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.send(weatherData);
-  }
-  catch (err) {
-    res.send(err);
-  }
-  
 });
