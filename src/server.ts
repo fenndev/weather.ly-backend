c3c74233ea86c575959bb5beb87486eb134e5fcc
fenndev <fenndev@proton.me>
2023-02-11 04:02:49 -0800

    Removed rate-limiting code from backend

--- src/server.ts
+++ src/server.ts
@@ -4,28 +4,22 @@ import queryWeather from './query-weather.js';
 import * as dotenv from 'dotenv';
 dotenv.config();
 import WeatherData from './classes/WeatherData.js';
-import sanitizeQuery from './functions/SanitizeQuery.js';
-import RateLimiter from './classes/RateLimiter.js';
 const app = express();
-let rateLimiter: RateLimiter;
 app.use(cors());
 
 app.get('/', async(req: any, res) => {
   if(!req.query.q) res.send('Server online.');
-  if(!rateLimiter.isRateLimitReached()) {
-    try {
-      let queryString = req.query.q;
-      let queryArray = queryString.split(',');
-      const weatherData = await queryWeather(queryArray);
-      res.send(weatherData);
-    }
-    catch (err) {
-      res.send(err);
-    }
+  try {
+    let queryString = req.query.q;
+    let queryArray = queryString.split(',');
+    const weatherData = await queryWeather(queryArray);
+    res.send(weatherData);
+  }
+  catch (err) {
+    res.send(err);
   }
 })
 
 app.listen(process.env.PORT, async() => {
     console.log(`Server started on port ${process.env.PORT}`);
-    rateLimiter = new RateLimiter();
 });
